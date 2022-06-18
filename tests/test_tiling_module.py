82091e723aa7d1a60d123df8cbdf34fd0942801b
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-06-18 13:37:18 -0600

    Add files via upload

--- tests/test_tiling_module.py
+++ tests/test_tiling_module.py
@@ -78,19 +78,23 @@ class TestTilingModule(BaseTest):
 
     def test_calc_tile_coords_1(self) -> None:
         tiling_module = TilingModule()
-        tile_dim=224
-        full_dim=512
-        overlap=0.5
-        coords, overlaps = tiling_module._calc_tile_coords(d=full_dim, tile_dim=tile_dim, overlap=overlap)
+        tile_dim = 224
+        full_dim = 512
+        overlap = 0.5
+        coords, overlaps = tiling_module._calc_tile_coords(
+            d=full_dim, tile_dim=tile_dim, overlap=overlap
+        )
         self.assertEqual(coords, [0, 112, 224, 288])
         self.assertEqual(overlaps, [[0, 112], [0, 112], [0, 112], [48, 112]])
 
     def test_calc_tile_coords_2(self) -> None:
         tiling_module = TilingModule()
-        tile_dim=112
-        full_dim=224
-        overlap=0.15
-        coords, overlaps = tiling_module._calc_tile_coords(d=full_dim, tile_dim=tile_dim, overlap=overlap)
+        tile_dim = 112
+        full_dim = 224
+        overlap = 0.15
+        coords, overlaps = tiling_module._calc_tile_coords(
+            d=full_dim, tile_dim=tile_dim, overlap=overlap
+        )
         self.assertEqual(coords, [0, 95, 112])
         self.assertEqual(overlaps, [[0, 17], [0, 17], [78, 17]])
 
